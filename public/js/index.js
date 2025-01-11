e4415dbbd0b8e48c1162e90321342d706cc3a9d6
Maicon Douglas <maicondouglas@Mac-mini-de-Maicon.local>
2025-01-11 11:37:00 -0300

    algumas adições =D

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 const form = document.getElementById('form')
+const botao = document.getElementById('botao');
+
+botao.addEventListener('mouseup', function() {
+    botao.classList.add('button-active'); 
+    });
+  
+    botao.addEventListener('mousedown', function() {
+        botao.classList.remove('button-active'); 
+    });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -25,4 +34,27 @@ form.addEventListener('submit', (event) => {
         status.innerHTML = 'Resultado: Erro ao publicar nomeee!'
         console.log('error')
     })
+})
+
+
+
+
+const interroga_btn = document.getElementById('interroga')
+
+interroga_btn.addEventListener('mouseover', function(){
+    const interroga_container = document.getElementById('interroga-container')
+    console.log('deu')
+    interroga_container.style.display = 'block'
+
+})
+interroga_btn.addEventListener('mouseout', function(){
+    const interroga_container = document.getElementById('interroga-container')
+    console.log('deu')
+    interroga_container.style.display = 'none'
+    
+})
+interroga_btn.addEventListener('click', function(){
+    const interroga_container = document.getElementById('interroga-container')
+    console.log('deu')
+    interroga_container.classList.toggle('hiddle')
 })
